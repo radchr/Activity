33561f601b582b1ee8f99aa79582c111b8e428cb
Radch <pochta.roman.r.git@gmail.com>
2024-07-19 21:50:46 +0300

    first add

---
+++ Rust/first_project/target/debug/deps/first_project-15dbdc50728c7e44.d
@@ -0,0 +1,5 @@
+C:\Dev\Activity\Rust\first_project\target\debug\deps\libfirst_project-15dbdc50728c7e44.rmeta: src/main.rs
+
+C:\Dev\Activity\Rust\first_project\target\debug\deps\first_project-15dbdc50728c7e44.d: src/main.rs
+
+src/main.rs:
